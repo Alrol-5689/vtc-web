e11e280c30f8240e444ec2c1862e87faea8cd30c
alex <alejandrocordobarodriguez@gmail.com>
2025-06-26 11:42:34 +0200

    hay que seguir cambiando cosas en Conductor tras la creación de Convenio

--- src/main/java/com/vtc/modelo/Conductor.java
+++ src/main/java/com/vtc/modelo/Conductor.java
@@ -1,21 +1,33 @@
 package com.vtc.modelo;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Conductor {
     
     // Puedes agregar atributos propios del Conductor aquí en el futuro, como Turno, gestor asignado, etc.
 	
 	private List<DiaConductor> diasTrabajados;
-    protected String dni, nombre, apellido1, apellido2, telefono, email, nick, password;
-	protected int id;
-	protected LocalDate fechaAlta;
+    private String dni, nombre, apellido1, apellido2, telefono, email, nick, password;
+	private int id;
+	private LocalDate fechaAlta;
+    private TreeMap<YearMonth, Map<DayOfWeek, Duration>> jornada;
+    private TreeMap<YearMonth, Duration> tareasAux;
+    private Duration tareasAux_porDef, jornadaDiaria_porDef, jornadaSemanal_porDef;
+
+    private static final Duration TAREAS_AUX_CONVENIO_2023 = Duration.ofMinutes(20);
+    private static final Duration TAREAS_AUX_CONVENIO_2024 = Duration.ofMinutes(20);
+    private static final Duration TAREAS_AUX_CONVENIO_2025 = Duration.ofMinutes(20);
+
 
     public Conductor(int id, String dni, String nombre, String apellido1, String apellido2, 
-    		String telefono, String email, String nick, String password, 
-    			Gestor gestorAsignado, Turno turno, LocalDate fechaAlta) {   	
+    		String telefono, String email, String nick, String password, LocalDate fechaAlta) {   	
         this.id = id;
         this.dni = dni;
         this.nombre = nombre;
@@ -34,6 +46,38 @@ public class Conductor {
 
     public void setDiasTrabajados(List<DiaConductor> diasTrabajados) {
         this.diasTrabajados = diasTrabajados;
+    }
+
+    public Duration getTareasAux_porDef() {
+        return tareasAux_porDef;
+    }
+
+    public void setTareasAux_porDef(Duration tareasAux_porDef) {
+        this.tareasAux_porDef = tareasAux_porDef;
+    }
+
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(LocalDate fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Duration getJornadaDiaria_porDef() {
+        return jornadaDiaria_porDef;
+    }
+
+    public void setJornadaDiaria_porDef(Duration jornada_porDef) {
+        this.jornadaDiaria_porDef = jornada_porDef;
+    }
+
+    public Duration getJornadaSemanal_porDef() {
+        return jornadaSemanal_porDef;
+    }
+
+    public void setJornadaSemanal_porDef(Duration jornadaSemanal_porDef) {
+        this.jornadaSemanal_porDef = jornadaSemanal_porDef;
     }
 }
 
