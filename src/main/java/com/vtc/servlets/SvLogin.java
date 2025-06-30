3dc2fe147a77a5f9fc5d204c2a6f8effbab03f38
alex <alejandrocordobarodriguez@gmail.com>
2025-06-30 19:25:23 +0200

    cambio de nombre de la clase

--- src/main/java/com/vtc/servlets/SvLogin.java
+++ src/main/java/com/vtc/servlets/SvLogin.java
@@ -7,20 +7,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet {
+public class SvLogin extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String usuario = request.getParameter("usuario");
         String password = request.getParameter("password");
         String tipo = request.getParameter("tipo");
 
-        // Aquí iría la lógica para autenticar al usuario (consultar base de datos, etc.)
+        // Aquí iría la lógica para autenticar al usuario (consultar base de datos,
+        // etc.)
         if ("admin".equals(usuario) && "1234".equals(password)) {
             response.sendRedirect("bienvenida.jsp");
         } else {
             response.sendRedirect("error.jsp");
         }
     }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
 }
