f243f106c4e4f4eb6c29732f0451a9572824d0da
alex <alejandrocordobarodriguez@gmail.com>
2025-07-15 19:05:48 +0200

    pruebas

--- src/main/java/com/vtc/mainapp/TestConexionBBDD.java
+++ src/main/java/com/vtc/mainapp/TestConexionBBDD.java
@@ -1,10 +1,17 @@
 package com.vtc.mainapp;
 
-import com.vtc.persistencia.ControladoraPersistencia;
+import com.vtc.modelo.Controladora;
 
 public class TestConexionBBDD {
     public static void main(String[] args) {
 
-        ControladoraPersistencia controladora = new ControladoraPersistencia();
+        Controladora control = new Controladora();
+
+        try {
+            control.eliminarDriver(4L);
+        } catch (Exception e) {
+            System.out.println("Error al crear el conductor: " + e.getMessage());
+        } 
+        
     }
 }
